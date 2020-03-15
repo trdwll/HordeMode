4531cb2524d4bf5f9d5449ec3f795338eef2cd6a
Russ Treadwell <git@trdwll.com>
2020-03-15 15:36:14 -0400

    Add kills and deaths to HMPlayerState

--- Source/HordeMode/Private/Player/HMPlayerState.cpp
+++ Source/HordeMode/Private/Player/HMPlayerState.cpp
@@ -2,4 +2,26 @@
 
 
 #include "Player/HMPlayerState.h"
+#include "Net/UnrealNetwork.h"
 
+AHMPlayerState::AHMPlayerState()
+	: m_Kills(0), m_Deaths(0)
+{
+
+}
+
+void AHMPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AHMPlayerState, m_Kills);
+	DOREPLIFETIME(AHMPlayerState, m_Deaths);
+}
+
+void AHMPlayerState::Reset()
+{
+	Super::Reset();
+
+	m_Kills = 0;
+	m_Deaths = 0;
+}
--- Source/HordeMode/Public/Player/HMPlayerState.h
+++ Source/HordeMode/Public/Player/HMPlayerState.h
@@ -14,4 +14,57 @@ class HORDEMODE_API AHMPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 
+
+
+public:
+	AHMPlayerState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void Reset() override;
+
+
+	/** --- Start HMPlayerState code --- */
+private:
+
+	/** The number of kills that the player has. */
+	UPROPERTY(Replicated)
+	int32 m_Kills;
+
+	/** The number of deaths that the player has. */
+	UPROPERTY(Replicated)
+	int32 m_Deaths;
+
+	///** The number of downs that the player has. */
+	//UPROPERTY(Replicated)
+	//int32 m_Downs;
+
+public:
+
+	/**
+	 * Add a kill to the playerstate.
+	 * This gets called on the server so no need to add a ServerRPC.
+	 */
+	FORCEINLINE void AddKill()
+	{
+		m_Kills++;
+	}
+
+	/**
+	 * Add a death to the playerstate.
+	 * This gets called on the server so no need to add a ServerRPC.
+	 */
+	FORCEINLINE void AddDeath()
+	{
+		m_Deaths++;
+	}
+
+	//void AddDown();
+
+	/** Get the kills of the playerstate. */
+	UFUNCTION(BlueprintPure, Category = "HMPlayerState")
+	FORCEINLINE int32 GetKills() const { return m_Kills; }
+
+	/** Get the deaths of the playerstate. */
+	UFUNCTION(BlueprintPure, Category = "HMPlayerState")
+	FORCEINLINE int32 GetDeaths() const { return m_Deaths; }
 };
